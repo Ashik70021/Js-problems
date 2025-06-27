1685c1d94064cb8e2eb39c76c06f921904919c64
Ashik70021 <ashik70021@gmail.com>
2025-06-27 15:57:58 +0600

    practice 2D array problems

--- array.js
+++ array.js
@@ -1,16 +1,43 @@
-// find max/min
-function findMaxMin(arr){
-    let max = arr[0];
-    let min = arr[0];
-    for(let num of arr){
-        if(num>max){
-            max = num;
-        }
-        else if(num<min){
-            min = num;
-        }
-    }
-    return {max, min};
-}
+// function print2Darr(matrix){
+//     for(let row of matrix){
+//         console.log(row.join(' '));
+//     }
+// }
+// print2Darr([
+//     [1,2,3],
+//     [4,2,3],
+//     [1,2,7]
+// ])
 
-console.log(findMaxMin([5,1,2,4,9]));
+// function sumdiagonal(matrix){
+//     let sum =0;
+//     for(let i=0; i<matrix.length; i++){
+//         console.log(matrix[i][i]);
+//         sum += matrix[i][i];
+//     }
+//     return sum;
+// }
+// console.log(sumdiagonal([
+//     [3,2,3],
+//     [4,2,3],
+//     [3,2,7]
+// ]));
+
+
+function sum2darr(matrix){
+    let sum = 0;
+    for(let row of matrix){
+        // console.log(row);
+        for(let num of row){
+            // console.log(num);
+            sum+=num;
+        }
+        
+    }
+    return sum;
+}
+console.log(sum2darr([
+    [5,2,4],
+    [4,8,9],
+    [7,5,3]
+]))
